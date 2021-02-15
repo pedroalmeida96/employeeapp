a80e43295539a995f34225ec322a01cfbb0d4b89
pedroalmeida96 <43607455+pgoaa@users.noreply.github.com>
2021-02-15 18:24:03 +0000

    all and add tested, working

---
+++ src/main/java/tech/pedroalmeida/employeemanager/controller/EmployeeController.java
@@ -0,0 +1,50 @@
+package tech.pedroalmeida.employeemanager.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tech.pedroalmeida.employeemanager.model.Employee;
+import tech.pedroalmeida.employeemanager.service.EmployeeService;
+import tech.pedroalmeida.employeemanager.utils.Mappings;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(Mappings.EMPLOYEES)
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping(Mappings.ALL)
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        List<Employee> employees = employeeService.getAllEmployees();
+        return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
+
+    @GetMapping(Mappings.FIND_BY_ID)
+    public ResponseEntity<Employee> getEmployee(@PathVariable("id") Long id) {
+        return new ResponseEntity(employeeService.getEmployee(id), HttpStatus.OK);
+    }
+
+    @PostMapping(Mappings.ADD_EMPLOYEE)
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
+        return new ResponseEntity(employeeService.addEmployee(employee), HttpStatus.CREATED);
+    }
+
+    @PutMapping(Mappings.UPDATE_EMPLOYEE)
+    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee) {
+        return new ResponseEntity(employeeService.updateEmployee(employee), HttpStatus.OK);
+    }
+
+    @DeleteMapping(Mappings.DELETE_EMPLOYEE)
+    public ResponseEntity<?> removeEmployee(@PathVariable("id") Long id) {
+        employeeService.removeEmployee(id);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+}
---
+++ src/main/java/tech/pedroalmeida/employeemanager/utils/Mappings.java
@@ -0,0 +1,14 @@
+package tech.pedroalmeida.employeemanager.utils;
+
+public class Mappings {
+
+    public static final String EMPLOYEES = "/employee";
+    public static final String ALL = "/all";
+    public static final String FIND_BY_ID = "/find/{id}";
+    public static final String ADD_EMPLOYEE = "/add";
+    public static final String UPDATE_EMPLOYEE = "/update";
+    public static final String DELETE_EMPLOYEE = "/delete/{id}";
+
+    public Mappings() {
+    }
+}
